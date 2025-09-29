3e4bd116598f72678646b2fbd939e3646fabfb56
Syddev <sydneysantos176@gmail.com>
2025-09-29 20:02:30 +0800

    imported recipe card component

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import Sidebar from "../components/SideBar";
 import Logo from "../assets/Logo.png";
-import Pasta from "../assets/pasta.jpg";
-import LikeRecipe from "../assets/Like Recipe.png";
-import ServingIcon from "../assets/Serving Icon.png";
-import Clock from "../assets/clock.png";
-import ViewRecipeIcon from "../assets/link.png";
+import RecipeCard from "../components/RecipeCard";
 import "../styles/Discover.css";
 
 const DiscoverRecipes = () => {
@@ -56,52 +52,7 @@ const DiscoverRecipes = () => {
             <p>Discover mouth-watering recipes from across the web</p>
           </div>
 
-          <div className="recipes">
-            <div className="recipe-box">
-              <div className="recipe-image-wrapper">
-                <img
-                  src={Pasta}
-                  alt="Classic Pasta Carbonara"
-                  className="recipe-picture"
-                />
-
-                <div className="recipe-like">
-                  <img src={LikeRecipe} alt="Like" className="like-icon" />
-                  <span>23</span>
-                </div>
-              </div>
-
-              <div className="recipe-content">
-                <h2 className="recipe-title">Classic Pasta Carbonara</h2>
-                <p className="recipe-desc">
-                  A creamy Roman pasta dish made with eggs, cheese, and ...
-                </p>
-                <div className="recipe-info">
-                  <span>
-                    <img src={Clock} alt="Time" className="info-icon" /> 25 mins
-                  </span>
-                  <span>
-                    <img
-                      src={ServingIcon}
-                      alt="Servings"
-                      className="info-icon"
-                    />
-                    2 servings
-                  </span>
-                </div>
-                <button className="view-recipe-btn">
-                  <img
-                    src={ViewRecipeIcon}
-                    alt="view-recipe"
-                    className="view-recipe-icon"
-                  />
-                  View Recipe
-                </button>
-
-                <p className="recipe-source">by Foodista.com</p>
-              </div>
-            </div>
-          </div>
+          <RecipeCard />
         </div>
       </div>
     </div>
