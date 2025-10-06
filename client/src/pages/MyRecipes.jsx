586360924ce3dc9a49a94ee9f01b45e3523bd6dd
Syddev <sydneysantos176@gmail.com>
2025-10-06 14:37:57 +0800

    added user instructions

--- client/src/pages/MyRecipes.jsx
+++ client/src/pages/MyRecipes.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Sidebar from "../components/Sidebar";
 import Logo from "../assets/Logo.png";
 import Pasta from "../assets/pasta.jpg";
-import LikeRecipe from "../assets/Like Recipe.png";
+import Heart from "../assets/Total Likes.png";
 import ServingIcon from "../assets/Serving Icon.png";
 import Clock from "../assets/clock.png";
 import Edit from "../assets/edit icon.png";
@@ -99,8 +99,15 @@ const MyRecipe = () => {
                   </div>
                   <div className="input-row-3">
                     <div className="input-group wide">
-                      <label htmlFor="ingredients">Ingredients *</label>
-                      <textarea id="ingredients" name="ingredients" required />
+                      <label htmlFor="ingredients">
+                        Ingredients (comma-seperated) *
+                      </label>
+                      <textarea
+                        id="ingredients"
+                        name="ingredients"
+                        placeholder="Example: pizza dough, mozzarella, tomatoes"
+                        required
+                      />
                     </div>
                     <div class="input-row">
                       <div className="input-group">
@@ -118,7 +125,16 @@ const MyRecipe = () => {
                 <div className="right-inputs">
                   <div className="input-group instructions-group">
                     <label htmlFor="instructions">Instructions *</label>
-                    <textarea id="instructions" name="instructions" required />
+                    <textarea
+                      id="instructions"
+                      name="instructions"
+                      placeholder="Enter each step with a number. Example:
+1. Preheat oven to 475°F (245°C)
+2. Roll out pizza dough
+3. Add sauce and cheese
+4. Bake for 12–15 minutes"
+                      required
+                    />
                   </div>
                   <button type="submit" className="share-recipe-btn">
                     Share Recipe
@@ -141,7 +157,7 @@ const MyRecipe = () => {
                   />
 
                   <div className="recipe-like">
-                    <img src={LikeRecipe} alt="Like" className="like-icon" />
+                    <img src={Heart} alt="Like" className="like-icon" />
                     <span>23</span>
                   </div>
                 </div>
