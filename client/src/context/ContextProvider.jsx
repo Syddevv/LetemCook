a7f0cf6aedc625413df206a325bafedd65b5db4d
Syddev <sydneysantos176@gmail.com>
2025-10-07 22:01:37 +0800

    created context provider

---
+++ client/src/context/ContextProvider.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { AuthContext } from "./authContext";
+
+const ContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const login = (user) => {
+    setUser(user);
+  };
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/auth/verify", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        if (res.data.success) {
+          setUser(res.data.user);
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    verifyUser();
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    window.location.reload();
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default ContextProvider;
