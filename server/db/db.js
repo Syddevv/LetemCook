90f15ffa2fa65422f669d373537b3b791cd018ce
Syddev <sydneysantos176@gmail.com>
2025-10-07 21:23:08 +0800

    changes the connection string to env

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/letemcook");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB connection error: ", error.message);
