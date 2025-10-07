78ac3b1a38e61ba665907a6313dd5610f3107289
Syddev <sydneysantos176@gmail.com>
2025-10-07 21:26:27 +0800

    port from env

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 import connectToMongoDB from "./db/db.js";
-import authRouter from "./routes/auth.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectToMongoDB();
   console.log("Server Up!");
 });
