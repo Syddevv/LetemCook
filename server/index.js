5135fb6e06fa9dfd7517555ffb02d34cb61a8b71
Syddev <sydneysantos176@gmail.com>
2025-10-07 22:03:46 +0800

    used dotenv config

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import connectToMongoDB from "./db/db.js";
 import authRouter from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
