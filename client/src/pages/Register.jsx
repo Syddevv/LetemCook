25cfe53e4f7f87662608e0cd12fbb860b27943c2
Syddev <sydneysantos176@gmail.com>
2025-11-17 19:26:06 +0800

    registration page responsiveness

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -128,6 +128,7 @@ const RegistrationPage = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.3 }}
+              className="form-inputs"
             >
               <div>
                 <label htmlFor="username">Username</label>
